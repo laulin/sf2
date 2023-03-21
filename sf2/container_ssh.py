7c1589d70bc2bb7b758e15be47ea5554447e8197
lmoulin <lmoulin@spartan-conseil.fr>
2023-03-21 18:58:56 +0100

    add comments

--- sf2/container_ssh.py
+++ sf2/container_ssh.py
@@ -20,6 +20,14 @@ class ContainerSSH(ContainerBase):
 
 
     def load_ssh_public_key(self, public_ssh_file:str)->None:
+        """
+        > This function takes a file path to a public ssh key file, reads the file, and returns the file
+        data, the user and host, and the public key
+        
+        :param public_ssh_file: The path to the public ssh key file
+        :type public_ssh_file: str
+        :return: The file data, the user host, and the public key.
+        """
 
         with open(public_ssh_file, "r") as f:
             file_data = f.read()
@@ -31,6 +39,15 @@ class ContainerSSH(ContainerBase):
     
     
     def load_ssh_private_key(self, private_ssh_file:str, password_private_ssh_file:bytes)->None:
+        """
+        This function loads a private ssh key from a file and returns it as a string
+        
+        :param private_ssh_file: The path to the private key file
+        :type private_ssh_file: str
+        :param password_private_ssh_file: The password for the private key file
+        :type password_private_ssh_file: bytes
+        :return: The private key is being returned.
+        """
 
         with open(private_ssh_file, "rb") as f:
             private_key = load_ssh_private_key(f.read(), password_private_ssh_file)
@@ -39,6 +56,20 @@ class ContainerSSH(ContainerBase):
 
 
     def add_ssh_key(self, password:str, public_ssh_file:str, auth_id:str=None, _iterations:int=None)->None:
+        """
+        It takes a master password, a public ssh key file, and an optional auth_id, and adds the public key
+        to the auth container, encrypting the master key with the public key
+        
+        :param password: The password you used to create the container
+        :type password: str
+        :param public_ssh_file: The path to the public key file
+        :type public_ssh_file: str
+        :param auth_id: The name of the user. This is used to identify the user, like test@computer. If not privided, it uses the one of the SSH key.
+        :type auth_id: str
+        :param _iterations: The number of iterations to use when generating the master key. If not
+        specified, the default value is used
+        :type _iterations: int
+        """
         container = self.load()
         master_key = self.get_master_key(container, password, _iterations)
         
@@ -68,6 +99,12 @@ class ContainerSSH(ContainerBase):
         self.dump(container)
 
     def remove_ssh_key(self, auth_id:str)->None:
+        """
+        This function removes the SSH key of a user
+        
+        :param auth_id: The user's auth_id
+        :type auth_id: str
+        """
         container = self.load()
 
         if auth_id in container["auth"]["users"] :
@@ -84,6 +121,19 @@ class ContainerSSH(ContainerBase):
         self.dump(container)
 
     def get_master_key_ssh(self, container:dict, auth_id:str, private_ssh_file:str, password_private_ssh_file:bytes):
+        """
+        The function decrypts the master key using the private key of the user.
+        
+        :param container: the container dictionary
+        :type container: dict
+        :param auth_id: The user's ID
+        :type auth_id: str
+        :param private_ssh_file: The path to the private key file
+        :type private_ssh_file: str
+        :param password_private_ssh_file: The password to decrypt the private ssh key
+        :type password_private_ssh_file: bytes
+        :return: The master key is being returned.
+        """
 
         private_key = self.load_ssh_private_key(private_ssh_file, password_private_ssh_file)
 
@@ -108,6 +158,19 @@ class ContainerSSH(ContainerBase):
     
     
     def get_master_data_key_ssh(self, container:dict, auth_id:str, private_ssh_file:str, password_private_ssh_file:bytes)->str:
+        """
+        It decrypts the master data key.
+        
+        :param container: the container dictionary
+        :type container: dict
+        :param auth_id: The ID of the authentication method you want to use
+        :type auth_id: str
+        :param private_ssh_file: The path to the private key file
+        :type private_ssh_file: str
+        :param password_private_ssh_file: The password for the private ssh file
+        :type password_private_ssh_file: bytes
+        :return: The master data key is being returned.
+        """
         master_key = self.get_master_key_ssh(container, auth_id, private_ssh_file, password_private_ssh_file)
 
         encrypted_master_data_key = self.b64decode(container["auth"]["encrypted_master_data_key"])
@@ -118,6 +181,17 @@ class ContainerSSH(ContainerBase):
 
 
     def read(self, auth_id:str, private_ssh_file:str, password_private_ssh_file:bytes=None)->bytes:
+        """
+        The function reads the encrypted data from the file using SSH KEY and returns the decrypted data
+        
+        :param auth_id: The ID of the authentication you want to use
+        :type auth_id: str
+        :param private_ssh_file: The path to the private key file
+        :type private_ssh_file: str
+        :param password_private_ssh_file: The password for the private ssh file
+        :type password_private_ssh_file: bytes
+        :return: The plain data.
+        """
         
         container = self.load()
 
@@ -127,6 +201,18 @@ class ContainerSSH(ContainerBase):
     
     
     def write(self, data:bytes, auth_id:str, private_ssh_file:str, password_private_ssh_file:bytes=None)->None:
+        """
+        It writes data to the container.
+        
+        :param data: the data to be encrypted
+        :type data: bytes
+        :param auth_id: The ID of the authentication key to use
+        :type auth_id: str
+        :param private_ssh_file: The path to the private key file
+        :type private_ssh_file: str
+        :param password_private_ssh_file: The password for the private ssh key file
+        :type password_private_ssh_file: bytes
+        """
 
         container = self.load()
 
