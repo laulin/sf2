bea39b06605de3580e8e8333b3006d483ac6c6ff
lmoulin <lmoulin@spartan-conseil.fr>
2023-04-16 16:44:33 +0200

    add ssh support

--- sf2/sf2.py
+++ sf2/sf2.py
@@ -1,8 +1,10 @@
-from getpass import getpass
+from getpass import getpass,getuser
 import sys
 import logging
 import os.path
 from pathlib import Path
+import re
+import socket
 
 from sf2.args import get_args
 from sf2.cipher import Cipher
@@ -50,10 +52,11 @@ class SF2:
             print("Usage --help for information")
             return
 
-        try:
-            commands[self._args.commands]()
-        except Exception as e:
-            self._log.critical(str(e))
+        commands[self._args.commands]()
+        # try:
+        #     commands[self._args.commands]()
+        # except Exception as e:
+        #     self._log.critical(str(e))
 
     def encrypt(self):
         base = self.get_format(self._args.outfilename)
@@ -84,9 +87,11 @@ class SF2:
 
             data = container.read(password)
         else:
-            container = ContainerSSH(support)
-            rsa_key_path = self.get_default_rsa_private_key()
-            data = container.read(self._args.auth_id, rsa_key_path, self._args.ssh_key_password)
+            base = ContainerBase(support)
+            container = ContainerSSH(base)
+            rsa_key_path = self.get_private_key()
+            auth_id = self.get_auth_id()
+            data = container.read(auth_id, rsa_key_path, self._args.ssh_key_password)
 
         with open(self._args.outfilename, "wb") as f:
             f.write(data)
@@ -94,7 +99,6 @@ class SF2:
     def verify(self):
         output = 0
         password = self.get_master_password()
-
         for filename in self._args.infilenames:
             support = self.get_format(filename)
             try:
@@ -102,9 +106,11 @@ class SF2:
                     container = ContainerBase(support)
                     container.read(password)
                 else:
-                    container = ContainerSSH(support)
-                    rsa_key_path = self.get_default_rsa_private_key()
-                    container.read(self._args.auth_id, rsa_key_path, self._args.ssh_key_password)
+                    base = ContainerBase(support)
+                    container = ContainerSSH(base)
+                    rsa_key_path = self.get_private_key()
+                    auth_id = self.get_auth_id()
+                    container.read(auth_id, rsa_key_path, self._args.ssh_key_password)
                 print(f"{filename} : OK")
             except Exception as e:
                 print(f"{filename} : KO ({e})")
@@ -116,7 +122,38 @@ class SF2:
         pass
 
     def ssh(self):
-        pass
+        commands = {
+            "add": self.ssh_add,
+            "rm": self.ssh_rm,
+            "ls": self.ssh_ls
+        }
+
+        commands[self._args.ssh_commands]()
+
+    def ssh_add(self):
+        password = self.get_master_password()
+        public_key_file = self.get_public_key()
+        auth_id = self.get_auth_id(public_key_file)
+        for filename in self._args.infilenames:
+            support = self.get_format(filename)
+            base = ContainerBase(support)
+            container = ContainerSSH(base)
+            container.add_ssh_key(password, public_key_file, auth_id)
+
+    def ssh_rm(self):
+        auth_id = self.get_auth_id()
+        for filename in self._args.infilenames:
+            support = self.get_format(filename)
+            container = ContainerSSH(support)
+            container.remove_ssh_key(auth_id)
+
+    def ssh_ls(self):
+        for filename in self._args.infilenames:
+            support = self.get_format(filename)
+            container = ContainerSSH(support)
+            print(f"{filename} :")
+            for user, pk in container.list_ssh_key().items():
+                print(user, pk)
 
     def get_format(self, filename:str):
         if self._args.format == "json":
@@ -130,17 +167,17 @@ class SF2:
         return str(Path.home())
     
     def get_default_rsa_private_key(self)->str:
-        return os.path.join([self.get_home(), ".ssh", "id_rsa"])
+        return os.path.join(self.get_home(), ".ssh", "id_rsa")
         
     def get_private_key(self):
-        if len(self._args.ssh_key) == 0:
+        if self._args.ssh_key_file is None:
             rsa_path = self.get_default_rsa_private_key()
             if os.path.exists(rsa_path):
                 return rsa_path
             else:
                 raise Exception(f"ssh key is not defined and {rsa_path} doesn't exist")
         else:
-            return self._args.ssh_key
+            return self._args.ssh_key_file
         
     def prevent_output_overwrite(self):
         if self._args.force == False:
@@ -148,10 +185,40 @@ class SF2:
                 raise Exception(f"Output file {self._args.outfilename} is already existing")
             
     def get_master_password(self)->str:
-        if not self._args.master_password_value:
-            return getpass()
-        else:
-            return self._args.master_password_value
+        try: 
+            if self._args.master_password:
+                if not self._args.master_password_value:
+                    return getpass()
+                else:
+                    return self._args.master_password_value
+        except AttributeError:
+            if not self._args.master_password_value:
+                return getpass()
+            else:
+                return self._args.master_password_value
+        
+    def get_public_key(self):
+        if self._args.public_key_file:
+            return self._args.public_key_file
+        
+        if os.path.exists(f"/home/{getuser()}/.ssh/id_rsa.pub"):
+            return f"/home/{getuser()}/.ssh/id_rsa.pub"
+        
+        raise Exception(f"Public key file is not provided and default one is not available (/home/{getuser()}/.ssh/id_rsa.pub)")
+    
+    def get_auth_id(self, public_key_file=None):
+        if self._args.auth_id:
+            return self._args.auth_id
+        
+        if public_key_file:
+            with open(public_key_file) as f:
+                key = f.read()
+                key = key.strip()
+
+            re_result = re.search(r"ssh-rsa AAAA[0-9A-Za-z+/]+[=]{0,3} ([^@]+@[^@\r\n]+)", key)
+            return re_result.group(1)
+        
+        return f"{getuser()}@{socket.gethostname()}"
 
 # def main():
 #     args = get_args()
--- test/test_cli.py
+++ test/test_cli.py
@@ -14,9 +14,6 @@ class TestCli(unittest.TestCase):
             pass
         os.mkdir(TEST_DIR)
 
-    def tearDown(self) -> None:
-        shutil.rmtree(TEST_DIR)
-
     def test_encrypt(self):
         source = os.path.join(TEST_DIR, "source.txt")
         encrypted = os.path.join(TEST_DIR, "encrypted.x")
@@ -45,6 +42,25 @@ class TestCli(unittest.TestCase):
 
         self.assertEqual(result, expected)
 
+    def test_decrypt_ssh(self):
+        source = os.path.join(TEST_DIR, "source.txt")
+        encrypted = os.path.join(TEST_DIR, "encrypted.x")
+        plain = os.path.join(TEST_DIR, "plain.txt")
+
+        with open(source, "w") as f:
+            f.write("Example ! ")
+
+        subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "ssh", "add", "-k", "test/.ssh/id_rsa.pub", "-m", "foobar", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "decrypt", "--ssh-key", "-y", "test/.ssh/id_rsa", "-a", "test@test", "-i", encrypted, "-o", plain])
+
+        with open(plain) as f:
+            result = f.read()
+
+        expected = "Example ! "
+
+        self.assertEqual(result, expected)
+
     def test_verify(self):
         source = os.path.join(TEST_DIR, "source.txt")
         encrypted = os.path.join(TEST_DIR, "encrypted.x")
@@ -54,6 +70,20 @@ class TestCli(unittest.TestCase):
 
         subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
         result = subprocess.check_output(["python3", "-m", "sf2", "verify", "--master-password", "-m", "foobar", encrypted])
+        expected = b'/tmp/test/encrypted.x : OK\n'
+
+        self.assertEqual(result, expected)
+
+    def test_verify_ssh(self):
+        source = os.path.join(TEST_DIR, "source.txt")
+        encrypted = os.path.join(TEST_DIR, "encrypted.x")
+
+        with open(source, "w") as f:
+            f.write("Example ! ")
+
+        subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "ssh", "add", "-k", "test/.ssh/id_rsa.pub", "-m", "foobar", encrypted])
+        result = subprocess.check_output(["python3", "-m", "sf2", "verify", "--ssh-key", "-y", "test/.ssh/id_rsa", "-a", "test@test", encrypted])
         expected = b'/tmp/test/encrypted.x : OK\n'
 
         self.assertEqual(result, expected)
@@ -75,3 +105,38 @@ class TestCli(unittest.TestCase):
         expected = b'/tmp/test/encrypted.x : KO (Master key is invalid)\n'
 
         self.assertEqual(result, expected)
+
+    def test_add_ssh(self):
+        source = os.path.join(TEST_DIR, "source.txt")
+        encrypted = os.path.join(TEST_DIR, "encrypted.x")
+
+        with open(source, "w") as f:
+            f.write("Example ! ")
+
+        subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
+        subprocess.check_output(["python3", "-m", "sf2", "ssh", "add", "-k", "test/.ssh/id_rsa.pub", "-m", "foobar", encrypted])
+
+    def test_rm_ssh(self):
+        source = os.path.join(TEST_DIR, "source.txt")
+        encrypted = os.path.join(TEST_DIR, "encrypted.x")
+
+        with open(source, "w") as f:
+            f.write("Example ! ")
+
+        subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "ssh", "add", "-k", "test/.ssh/id_rsa.pub", "-m", "foobar", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "ssh", "rm", "-a", "test@test", encrypted])
+
+    def test_ls_ssh(self):
+        source = os.path.join(TEST_DIR, "source.txt")
+        encrypted = os.path.join(TEST_DIR, "encrypted.x")
+
+        with open(source, "w") as f:
+            f.write("Example ! ")
+
+        subprocess.call(["python3", "-m", "sf2", "encrypt", "-m", "foobar", "-i", source, "-o", encrypted])
+        subprocess.call(["python3", "-m", "sf2", "ssh", "add", "-k", "test/.ssh/id_rsa.pub", "-m", "foobar", encrypted])
+        result = subprocess.check_output(["python3", "-m", "sf2", "ssh", "ls", encrypted])
+
+        lines = result.splitlines()
+        self.assertEqual(len(lines), 2 )
