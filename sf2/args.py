d8351657e39cea0f43b07373521fb16f434d16f4
lmoulin <lmoulin@spartan-conseil.fr>
2023-04-16 15:02:12 +0200

    change args

--- sf2/args.py
+++ sf2/args.py
@@ -18,34 +18,20 @@ By Laulin
 Supported by Spartan Conseil
 """
 
-# def get_args():
-#     parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=RawTextHelpFormatter)
-#     action = parser.add_mutually_exclusive_group(required=True)
-#     action.add_argument('-e', "--encrypt", action='store_true', default=False, dest='encrypt', help='Encrypt a file')
-#     action.add_argument('-d', "--decrypt", action='store_true', default=False, dest='decrypt', help='Decrypt a file. If -o is not provided, stdout is used')
-#     action.add_argument('--verify', action='store_true', default=False, dest='verify', help='Check if the key is valid on the file')
-#     action.add_argument('-n', "--new", action='store_true', default=False, dest='new', help='Create an empty encrypted file')
-#     action.add_argument('--edit', action='store_true', default=False, dest='edit', help='Run the external editor')
-#     action.add_argument('--gui', action='store_true', default=False, dest='gui', help='Run the graphical version')
-
-#     parser.add_argument('-i', "--in", default=None, dest='infilename', help='Select the encrypt file pass')
-#     parser.add_argument('-o', "--out", default=None, dest='outfilename', help='Select the encrypt file pass')
-#     parser.add_argument('--editor', default="mousepad", dest='editor', help='Select the external editor')
-#     parser.add_argument("-v", "--verbose", dest="verbosity", action="count", default=0,
-#                         help="Verbosity (between 1-4 occurrences with more leading to more "
-#                             "verbose logging). CRITICAL=0, ERROR=1, WARN=2, INFO=3, "
-#                             "DEBUG=4")
-#     args =  parser.parse_args()
-
-#     if not args.gui and args.infilename is None:
-#         parser.error("-i is expected")
-
-#     return args
 
 def create_exclusive_secret(subparser):
     secret_choice = subparser.add_mutually_exclusive_group(required=True)
-    secret_choice.add_argument('--master-password', action='store_true', dest='master_password', help='Use a master password parameters. Will call a prompt.')
-    secret_choice.add_argument('--ssh-key', action='store', nargs='?', const='', dest="ssh_key", help='Provide path a secret ssh key. If no path is provided, use default SSH key')
+    secret_choice.add_argument('--master-password', action='store_true', default=False, dest='master_password', help='Use a master password parameters. Will call a prompt.')
+    secret_choice.add_argument('--ssh-key', action='store_true', default=False, dest="ssh_key", help='Provide path a secret ssh key. If no path is provided, use default SSH key')
+
+    subparser.add_argument('-a', "--auth_id", required=False, dest='auth_id', help='Select the authentication id, need for SSH')
+    subparser.add_argument('-K', action='store', required=False, dest="ssh_key_password", help='Provide the password to unlock the private key')
+    subparser.add_argument('-y', action='store', required=False, dest="ssh_key_file", help='Provide the private key')
+
+    add_master_password(subparser)
+
+def add_master_password(subparser):
+    subparser.add_argument('-m', action='store', required=False, dest="master_password_value", help='Provide the master password')
 
 def add_log(subparser):
     subparser.add_argument("-v", "--verbose", dest="verbosity", action="count", default=0,
@@ -58,11 +44,7 @@ def add_format(subparser):
 
 def add_format_and_tail_file(subparser):
     add_format(subparser)
-    subparser.add_argument('infilename', nargs=argparse.REMAINDER)
-
-def add_private_key_options(subparser):
-    subparser.add_argument('-a', "--auth_id", required=False, default=None, dest='auth_id', help='Select the authentication id, need for SSH')
-    subparser.add_argument('-K', action='store', nargs='?', const=None, required=False, dest="ssh_key_password", help='Provide the password to unlock the private key')
+    subparser.add_argument('infilenames', nargs=argparse.REMAINDER)
 
 def add_io(subparser):
     subparser.add_argument('-i', "--in", required=True, default=None, dest='infilename', help='Select the plain file path')
@@ -79,6 +61,7 @@ def get_args(cli_args=None):
 
     # encrypt
     encrypt_parser = subparsers.add_parser('encrypt', help='Encrypt a file')
+    add_master_password(encrypt_parser)
     add_log(encrypt_parser)
     add_io(encrypt_parser)
     add_format(encrypt_parser)
@@ -87,7 +70,6 @@ def get_args(cli_args=None):
     decrypt_parser = subparsers.add_parser('decrypt',  help='Decrypt a file')
     create_exclusive_secret(decrypt_parser)
     add_log(decrypt_parser)
-    add_private_key_options(decrypt_parser)
     add_io(decrypt_parser)
     add_format(decrypt_parser)
 
@@ -101,7 +83,6 @@ def get_args(cli_args=None):
     open_parser = subparsers.add_parser('open', help='Open an encrypted file with external software')
     create_exclusive_secret(open_parser)
     add_log(open_parser)
-    add_private_key_options(open_parser)
     add_program(open_parser)
     add_format_and_tail_file(open_parser)
 
@@ -109,7 +90,6 @@ def get_args(cli_args=None):
     verify_parser = subparsers.add_parser('verify',  help='verify a file')
     create_exclusive_secret(verify_parser)
     add_log(verify_parser)
-    add_private_key_options(verify_parser)
     add_format_and_tail_file(verify_parser)
 
     # ssh
@@ -120,6 +100,7 @@ def get_args(cli_args=None):
     add_log(add_ssh_parser)
     add_ssh_parser.add_argument("-k", '--public', required=False, default=None, dest='public_key', help='Select the public key. Default is ~/{curent_user}/.ssh/id_rsa.pub')
     add_ssh_parser.add_argument("-a", '--auth-id', required=False, default=None, dest='auth_id', help='Define the authentification id, default is the one in the public key')
+    add_master_password(add_ssh_parser)
     add_format_and_tail_file(add_ssh_parser)
     # ssh remove
     rm_ssh_parser = ssh_subparser.add_parser('rm',  help='Remove an ssh key from a container')
--- test/test_args.py
+++ test/test_args.py
@@ -5,7 +5,7 @@ from sf2.args import get_args
 
 
 
-class TestGetArgs(unittest.TestCase):
+class TestArgs(unittest.TestCase):
 
     # def test_help(self):
     #     # args = get_args(["encrypt", "--help"])
@@ -21,9 +21,9 @@ class TestGetArgs(unittest.TestCase):
         self.assertEqual(results, expected)
 
     def test_encrypt_format(self):
-        args = get_args(["encrypt", "--format", "json", "-i", "in.txt", "-o", "out.x"])
-        results = [args.infilename, args.outfilename, args.format]
-        expected = ["in.txt", "out.x", "json"]
+        args = get_args(["encrypt", "--format", "json", "-m", "foobar", "-i", "in.txt", "-o", "out.x"])
+        results = [args.infilename, args.outfilename, args.format, args.master_password_value]
+        expected = ["in.txt", "out.x", "json", "foobar"]
 
         self.assertEqual(results, expected)
 
@@ -31,111 +31,118 @@ class TestGetArgs(unittest.TestCase):
 
     def test_decrypt_mk(self):
         args = get_args(["decrypt", "--master-password", "-i", "in.txt", "-o", "out.x"])
-        results = [args.infilename, args.outfilename, args.decryptpassword]
-        expected = ["in.txt", "out.x", True]
+        results = [args.infilename, args.outfilename, args.master_password, args.ssh_key]
+        expected = ["in.txt", "out.x", True, False]
+
+        self.assertEqual(results, expected)
+
+    def test_decrypt_mk_with_master_password(self):
+        args = get_args(["decrypt", "--master-password", "-m", "foobar", "-i", "in.txt", "-o", "out.x"])
+        results = [args.infilename, args.outfilename, args.master_password, args.master_password_value]
+        expected = ["in.txt", "out.x", True, "foobar"]
 
         self.assertEqual(results, expected)
 
     def test_decrypt_ssh_key_no_file(self):
         args = get_args(["decrypt", "--ssh-key", "-i", "in.txt", "-o", "out.x"])
         results = [args.infilename, args.outfilename, args.ssh_key]
-        expected = ["in.txt", "out.x", ""]
+        expected = ["in.txt", "out.x", True]
 
         self.assertEqual(results, expected)
 
     def test_decrypt_ssh_key_with_file(self):
-        args = get_args(["decrypt", "--ssh-key", "/home/foo/.ssh/id_rsa", "-i", "in.txt", "-o", "out.x"])
-        results = [args.infilename, args.outfilename, args.ssh_key]
-        expected = ["in.txt", "out.x", "/home/foo/.ssh/id_rsa"]
+        args = get_args(["decrypt", "--ssh-key", "-y", "/home/foo/.ssh/id_rsa", "-K", "foobar", "-i", "in.txt", "-o", "out.x"])
+        results = [args.infilename, args.outfilename, args.ssh_key, args.ssh_key_file, args.ssh_key_password]
+        expected = ["in.txt", "out.x", True, "/home/foo/.ssh/id_rsa", "foobar"]
 
         self.assertEqual(results, expected)
 
-    # convert
+    # # # convert
     def test_decrypt_mk(self):
         args = get_args(["convert", "-i", "in.y", "-o", "out.x"])
         results = [args.infilename, args.outfilename]
         expected = ["in.y", "out.x"]
         self.assertEqual(results, expected)
 
-    # open
+    # # # open
     def test_open_mk(self):
         args = get_args(["open", "--master-password", "--program", "nano",  "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password, args.program]
+        results = [args.commands, args.infilenames, args.master_password, args.program]
         expected = ['open', ['out.x'], True, 'nano']
 
         self.assertEqual(results, expected)
 
     def test_open_ssh_key_no_file(self):
         args = get_args(["open", "--ssh-key", "--program", "nano",  "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password, args.program, args.ssh_key]
-        expected = ['open', ['out.x'], False, 'nano', '']
+        results = [args.commands, args.infilenames, args.ssh_key, args.program, args.ssh_key_file]
+        expected = ['open', ['out.x'], True, 'nano', None]
 
         self.assertEqual(results, expected)
 
     def test_open_ssh_key_with_file(self):
-        args = get_args(["open", "--ssh-key", ".ssh/id_rsa", "--program", "nano",  "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password, args.program, args.ssh_key]
-        expected = ['open', ['out.x'], False, 'nano', ".ssh/id_rsa"]
+        args = get_args(["open", "--ssh-key", "-y", ".ssh/id_rsa", "--program", "nano",  "out.x"]) 
+        results = [args.commands, args.infilenames, args.ssh_key, args.program, args.ssh_key_file]
+        expected = ['open', ['out.x'], True, 'nano', ".ssh/id_rsa"]
 
         self.assertEqual(results, expected)
 
-    # Currently disable, need to implement configuration
-    # def test_open_without_args(self):
-    #     args = get_args(["open", "out.x"]) 
-    #     results = [args.commands, args.infilename, args.master_password, args.program, args.ssh_key]
-    #     expected = ['open', ['out.x'], False, None, None]
+    # # Currently disable, need to implement configuration
+    # # def test_open_without_args(self):
+    # #     args = get_args(["open", "out.x"]) 
+    # #     results = [args.commands, args.infilename, args.master_password, args.program, args.ssh_key]
+    # #     expected = ['open', ['out.x'], False, None, None]
 
-    #     self.assertEqual(results, expected)
+    # #     self.assertEqual(results, expected)
 
     # verify
     def test_verify_mk(self):
         args = get_args(["verify", "--master-password",  "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password]
+        results = [args.commands, args.infilenames, args.master_password]
         expected = ['verify', ['out.x'], True]
 
         self.assertEqual(results, expected)
 
     def test_verify_ssh_key_no_file(self):
-        args = get_args(["verify", "--ssh-key", "", "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password, args.ssh_key]
-        expected = ['verify', ['out.x'], False, '']
+        args = get_args(["verify", "--ssh-key", "out.x"]) 
+        results = [args.commands, args.infilenames, args.ssh_key]
+        expected = ['verify', ['out.x'], True]
 
         self.assertEqual(results, expected)
 
     def test_verify_ssh_key_with_file(self):
-        args = get_args(["verify", "--ssh-key", ".ssh/id_rsa", "out.x"]) 
-        results = [args.commands, args.infilename, args.master_password, args.ssh_key]
-        expected = ['verify', ['out.x'], False, ".ssh/id_rsa"]
+        args = get_args(["verify", "--ssh-key", "-y", ".ssh/id_rsa", "out.x"]) 
+        results = [args.commands, args.infilenames, args.ssh_key, args.ssh_key_file]
+        expected = ['verify', ['out.x'], True, ".ssh/id_rsa"]
 
         self.assertEqual(results, expected)
 
-    # ssh add
+    # # ssh add
     def test_ssh_add_all(self):
         args = get_args(["ssh", "add", "-k", ".ssh/id_rsa.pub", "-a", "foo@bar", "out.x"]) 
-        results = [args.commands, args.ssh_commands, args.public_key, args.auth_id, args.infilename]
+        results = [args.commands, args.ssh_commands, args.public_key, args.auth_id, args.infilenames]
         expected = ['ssh', "add", ".ssh/id_rsa.pub", "foo@bar", ["out.x"]]
 
         self.assertEqual(results, expected)
 
     def test_ssh_add_default(self):
-        args = get_args(["ssh", "add", "-k", "", "out.x"]) 
-        results = [args.commands, args.ssh_commands, args.public_key, args.auth_id, args.infilename]
-        expected = ['ssh', "add", "", None, ["out.x"]]
+        args = get_args(["ssh", "add", "out.x"]) 
+        results = [args.commands, args.ssh_commands, args.public_key, args.auth_id, args.infilenames]
+        expected = ['ssh', "add", None, None, ["out.x"]]
 
         self.assertEqual(results, expected)
 
     # ssh rm
-    def test_ssh_add_all(self):
+    def test_ssh_rm(self):
         args = get_args(["ssh", "rm", "-a", "foo@bar", "out.x"]) 
-        results = [args.commands, args.ssh_commands, args.auth_id, args.infilename]
-        expected = ['ssh', "rm", ".ssh/id_rsa.pub", "foo@bar", ["out.x"]]
+        results = [args.commands, args.ssh_commands, args.auth_id, args.infilenames]
+        expected = ['ssh', "rm", "foo@bar", ["out.x"]]
 
         self.assertEqual(results, expected)
 
     # ssh ls
-    def test_ssh_add_all(self):
+    def test_ssh_ls(self):
         args = get_args(["ssh", "ls", "out.x"]) 
-        results = [args.commands, args.ssh_commands, args.infilename]
+        results = [args.commands, args.ssh_commands, args.infilenames]
         expected = ['ssh', "ls",  ["out.x"]]
 
         self.assertEqual(results, expected)
