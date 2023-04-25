75a2f327f8a1534bf43dbedf11c6e4b1b00fe738
lmoulin <lmoulin@spartan-conseil.fr>
2023-04-25 17:39:25 +0200

    add tool

--- sf2/gui/tools.py
+++ sf2/gui/tools.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
+from pywebio import *
+
 from sf2.json_support import JsonSupport
 from sf2.msgpack_support import MsgpackSupport
-
 
 RED = '#ff2121'
 ORANGE = '#ff8821'
@@ -11,3 +14,20 @@ def get_format(support_format:str, filename:str):
         return JsonSupport(filename)
     elif support_format == "msgpack":
         return MsgpackSupport(filename)
+    
+def check_input_output_file(input_name, output_name):
+    infilename = pin.pin[input_name]
+    try:
+        Path(infilename).resolve()
+        with open(infilename):
+            pass
+    except (OSError, RuntimeError) as e:
+        raise Exception(f"Input file is invalid ({e})")
+
+    outfilename = pin.pin[output_name]
+    try:
+        Path(outfilename).resolve()
+    except (OSError, RuntimeError) as e:
+        raise Exception(f"Output file is invalid ({e})")
+    
+    return infilename, outfilename
