d1121da0effb41e0074d863814698db0e81b589e
lmoulin <lmoulin@spartan-conseil.fr>
2023-05-07 16:11:58 +0200

    change licence, description and exclude list in setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(name='sf2',
     license='MIT',
     author='Laurent MOULIN',
     author_email='gignops@gmail.com',
-    description='Encrypt and decrypt your file with Fernet algorithm',
-    packages=find_packages(exclude=['tests', "etc", "build", "dist", "sf2.egg-info"]),
+    description='Share and work in team your files in full safety thanks to SSH keys.',
+    packages=find_packages(exclude=['test', "etc", "build", "dist", "sf2.egg-info", "bin"]),
     install_requires=["cryptography", "inotify", "msgpack", "flufl.lock"],
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
     classifiers=[
       "Programming Language :: Python :: 3",
-      "License :: OSI Approved :: Apache Software License",
+      "License :: OSI Approved :: MIT LICENSE",
       "Operating System :: OS Independent",
       "Development Status :: 5 - Production/Stable",
       "Intended Audience :: Information Technology",
@@ -23,8 +23,7 @@ setup(name='sf2',
     python_requires='>=3',
       entry_points={
             'console_scripts': [ 
-            'sf2 = sf2.sf2:main', 
-            'sf2gui = sf2.sf2gui:main'
+            'sf2 = sf2.sf2:main'
             ] 
       }
 )
